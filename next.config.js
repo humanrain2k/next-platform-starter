60fd839e166c36e88c89adb83be8e70e991ecf48
humanrain2k <49112666+humanrain2k@users.noreply.github.com>
2024-12-03 11:01:02 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,11 @@
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
-    output: 'standalone'
+    output: 'export',
+    images: {
+        unoptimized: true,
+    },
+    trailingSlash: true,
 };
 
 module.exports = nextConfig;
